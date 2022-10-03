15b2f57fbf9574e3bc99d7e06c6c730b92dfe3af
emily40830 <emily40830@gmail.com>
2022-10-03 23:33:18 +0800

    feat: implement get todos

--- src/api/todos.js
+++ src/api/todos.js
@@ -2,7 +2,12 @@ import axios from 'axios';
 
 const baseUrl = 'http://localhost:3004';
 
-const getTodos = () => {};
+export const getTodos = () => {
+  return axios
+    .get(`${baseUrl}/todos`)
+    .then((res) => res.data)
+    .catch((err) => console.error('[Get Todos failed]: ', err));
+};
 
 const createTodo = () => {};
 
