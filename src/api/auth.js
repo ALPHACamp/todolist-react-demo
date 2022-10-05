b5147494f31fc8f1dc0c9eab596aeea7061d39b8
emily40830 <emily40830@gmail.com>
2022-10-05 23:11:51 +0800

    feat: add auth api

---
+++ src/api/auth.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const authURL = 'https://webdev.alphacamp.io/api/auth';
+
+export const register = async ({ username, email, password }) => {
+  const { data } = await axios.post(`${authURL}/register`, {
+    username,
+    email,
+    password,
+  });
+
+  return data;
+};
+
+export const login = async (data) => {
+  const res = await axios.post(`${authURL}/login`, {
+    username: data.username,
+    password: data.password,
+  });
+
+  return res.data;
+};
+
+export const logout = () => {
+  localStorage.removeItem('authToken');
+};
+
+export const checkPermission = async (authToken) => {
+  try {
+    const res = await axios.get(`${authURL}/test-token`, {
+      headers: {
+        Authorization: 'Bearer ' + authToken,
+      },
+    });
+
+    return res.data.success;
+  } catch (error) {
+    console.error(error);
+  }
+};
