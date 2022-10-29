07d6a3ab7a92a577002f654732429e38e0f23da8
emily40830 <emily40830@gmail.com>
2022-10-29 17:38:56 +0800

    feat: add register and check permission function

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -4,18 +4,50 @@ const authURL = 'https://todo-list.alphacamp.io/api/auth';
 
 export const login = async (data) => {
   try {
-    const res = await axios.post(`${authURL}/login`, {
+    const { data } = await axios.post(`${authURL}/login`, {
       username: data.username,
       password: data.password,
     });
 
-    const { authToken } = res.data;
+    const { authToken } = data;
 
     if (authToken) {
-      return { success: true, ...res.data };
+      return { success: true, ...data };
     }
-    return res.data;
+    return data;
   } catch (error) {
     console.error('[Login Failed]:', error);
+  }
+};
+
+export const register = async ({ username, email, password }) => {
+  try {
+    const { data } = await axios.post(`${authURL}/register`, {
+      username,
+      email,
+      password,
+    });
+    const { authToken } = data;
+    if (authToken) {
+      return { success: true, ...data };
+    }
+
+    return data;
+  } catch (error) {
+    console.error('[Register Failed]: ', error);
+  }
+};
+
+export const checkPermission = async (authToken) => {
+  try {
+    const { data } = await axios.get(`${authURL}/test-token`, {
+      headers: {
+        Authorization: 'Bearer ' + authToken,
+      },
+    });
+
+    return data.success;
+  } catch (error) {
+    console.error('[Forbidden]', error);
   }
 };
