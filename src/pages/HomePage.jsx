9debeb708e9d42b77231c2fe8d420e9c7b1be627
emily40830 <emily40830@gmail.com>
2022-10-30 00:30:16 +0800

    feat: add navigate at home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { checkPermission } from 'apis/auth';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkTokenIsValid = async () => {
+      const authToken = localStorage.getItem('authToken');
+      if (!authToken) {
+        navigate('/login');
+      }
+      const result = await checkPermission(authToken);
+      if (result) {
+        navigate('/todos');
+      } else {
+        navigate('/login');
+      }
+    };
+
+    checkTokenIsValid();
+  }, [navigate]);
 };
 
 export default HomePage;
